a0ca5803edadc40a8533059651ad64ecdc913177
Cameron A. Craig <camieac@gmail.com>
2017-02-17 12:17:59 +0000

    Added compose and chatr modals.

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -1,25 +1,31 @@
-// Get the modal
-var modal = document.getElementById('chat_modal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("chat_button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+function setup_modal(modal, button, close) {
+  // When the user clicks the button, open the modal 
+  button.onclick = function() {
     modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  close.onclick = function() {
+    modal.style.display = "none";
+  }
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// Get the modal
+var compose_modal = document.getElementById('compose_modal');
+var compose_btn = document.getElementById("compose_button");
+var compose_modal_close = document.getElementById("compose_modal_close");
+setup_modal(compose_modal, compose_btn, compose_modal_close);
+
+var chat_modal = document.getElementById('chat_modal');
+var chat_btn = document.getElementById("chat_button");
+var chat_modal_close = document.getElementById("chat_modal_close");
+setup_modal(chat_modal, chat_btn, chat_modal_close);
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+  window.onclick = function(event) {
+    if (event.target == compose_modal) {
+      compose_modal.style.display = "none";
+    } else if(event.target == chat_modal){
+    	chat_modal.style.display = "none"
     }
-}
+  }
