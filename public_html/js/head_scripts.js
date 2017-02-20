2a34622456ad61bc7d07957a4aba08279ab50039
Cameron A. Craig <camieac@gmail.com>
2017-02-20 20:03:03 +0000

    Messages now dissapear after 4s.

--- public_html/js/head_scripts.js
+++ public_html/js/head_scripts.js
@@ -20,3 +20,19 @@ function get_email_row_html(email) {
 	email_html += "</tr>";
 	return email_html;
 }
+
+/* Called when login button is pressed, before data is passed to server. */
+function validate_login() {
+	// Validate username
+	var username = document.forms["login_form"]["username"].value;
+	if(username == "") {
+		message("error", "Username cannot be blank!");
+		form.username.focus();
+		return false;
+    }
+	var password = document.forms["login_form"]["password"].value;
+	if (password == "") {
+		message("error", "Password must be fiilled out!");
+		return false;
+	}
+}
