888a016864b743472f3c013663b26c12a6ce06db
aidan <aidang123@googlemail.com>
2017-02-20 17:41:14 +0000

    Settings bar now had drop down menu

--- public_html/js/head_scripts.js
+++ public_html/js/head_scripts.js
@@ -20,3 +20,25 @@ function get_email_row_html(email) {
 	email_html += "</tr>";
 	return email_html;
 }
+
+/***************Settings button****************************/
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+/********************************************/
