209498bd0602b649f897d86d5261e0961d54767a
Cameron A. Craig <camieac@gmail.com>
2017-02-23 19:26:13 +0000

    Hide all email forms on modal close.

---
+++ public_html/js/email.js
@@ -0,0 +1,136 @@
+function show_reply_form() {
+	hide_read_email_buttons();
+	hide_forward_form();
+	hide_reply_all_form();
+
+	//Display the reply text area
+	document.getElementById("reply_content").style.display = "inline";
+
+	//Display the reply buttons
+	document.getElementById("send_reply_btn").style.display = "inline";
+	document.getElementById("cancel_reply_btn").style.display = "inline";
+}
+
+/* These are used by reply/reply all/forward functions below */
+function hide_read_email_buttons() {
+	//Hide the buttons that are no longer relevant
+	document.getElementById("delete_btn").style.display = "none";
+	document.getElementById("reply_btn").style.display = "none";
+	document.getElementById("reply_all_btn").style.display = "none";
+	document.getElementById("forward_btn").style.display = "none";
+}
+
+function show_read_email_buttons() {
+	//Hide the buttons that are no longer relevant
+	document.getElementById("delete_btn").style.display = "inline";
+	document.getElementById("reply_btn").style.display = "inline";
+	document.getElementById("reply_all_btn").style.display = "inline";
+	document.getElementById("forward_btn").style.display = "inline";
+}
+
+function hide_reply_form(){
+	show_read_email_buttons();
+	//Display the reply text area
+	document.getElementById("reply_content").style.display = "none";
+	//Hide all the stuff we shown for the reply option
+	//Display the reply buttons
+	document.getElementById("send_reply_btn").style.display = "none";
+	document.getElementById("cancel_reply_btn").style.display = "none";
+}
+
+function show_reply_all_form(){
+	hide_read_email_buttons();
+	hide_forward_form();
+	hide_reply_form();
+	//Display the reply text area
+	document.getElementById("reply_all_content").style.display = "inline";
+
+	//Display the reply buttons
+	document.getElementById("send_reply_all_btn").style.display = "inline";
+	document.getElementById("cancel_reply_all_btn").style.display = "inline";
+}
+
+function hide_reply_all_form(){
+	show_read_email_buttons();
+	//Display the reply text area
+	document.getElementById("reply_all_content").style.display = "none";
+	//Hide all the stuff we shown for the reply option
+	//Display the reply buttons
+	document.getElementById("send_reply_all_btn").style.display = "none";
+	document.getElementById("cancel_reply_all_btn").style.display = "none";
+}
+
+function hide_reply_all_form(){
+	show_read_email_buttons();
+	hide_forward_form();
+	hide_reply_form();
+	//Display the reply text area
+	document.getElementById("reply_all_content").style.display = "none";
+	//Hide all the stuff we shown for the reply option
+	//Display the reply buttons
+	document.getElementById("send_reply_all_btn").style.display = "none";
+	document.getElementById("cancel_reply_all_btn").style.display = "none";
+}
+
+function show_forward_form() {
+	hide_read_email_buttons();
+
+	//Display the reply text area
+	document.getElementById("forward_content").style.display = "inline";
+
+	//Display the reply buttons
+	document.getElementById("send_forward_btn").style.display = "inline";
+	document.getElementById("cancel_forward_btn").style.display = "inline";
+}
+
+function hide_forward_form() {
+	show_read_email_buttons()
+
+	//Display the reply text area
+	document.getElementById("forward_content").style.display = "none";
+	//Hide all the stuff we shown for the reply option
+	//Display the reply buttons
+	document.getElementById("send_forward_btn").style.display = "none";
+	document.getElementById("cancel_forward_btn").style.display = "none";
+}
+
+function hide_all_email_forms() {
+	hide_reply_form();
+	hide_forward_form();
+	hide_reply_all_form();
+}
+
+/* Reply stuff */
+var reply_button = document.getElementById('reply_btn');
+reply_button.onclick = function() {
+	show_reply_form();
+}
+
+var cancel_reply_button = document.getElementById('cancel_reply_btn');
+cancel_reply_button.onclick = function() {
+	hide_reply_form();
+}
+
+
+/* Reply all stuff */
+var reply_all_button = document.getElementById('reply_all_btn');
+reply_all_button.onclick = function() {
+	show_reply_all_form();
+}
+
+var cancel_reply_all_button = document.getElementById('cancel_reply_all_btn');
+cancel_reply_all_button.onclick = function() {
+	hide_reply_all_form();
+}
+
+
+/* Forward stuff */
+var forward_button = document.getElementById('forward_btn');
+forward_button.onclick = function() {
+	show_forward_form();
+}
+
+var cancel_forward_button = document.getElementById('cancel_forward_btn');
+cancel_forward_button.onclick = function() {
+	hide_forward_form();
+}
--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -10,6 +10,7 @@ function setup_modal(modal, button, close) {
     // When the user clicks on <span> (x), close the modal
     close.onclick = function() {
         modal.style.display = "none";
+				hide_all_email_forms();
     }
 }
 
@@ -80,6 +81,7 @@ function display_message(email_message_json) {
     // When the user clicks on <span> (x), close the modal
     close_button.onclick = function() {
         modal.style.display = "none";
+				hide_all_email_forms();
     }
 
     var subject = document.getElementById('read_email_subject');
@@ -127,7 +129,9 @@ window.onclick = function(event) {
     } else if (event.target == chat_modal) {
         chat_modal.style.display = "none";
     } else if (event.target == advanced_search_modal) {
-        chat_modal.style.display = "none";
+        advanced_search_modal.style.display = "none";
+    } else if (event.target == help_modal) {
+        help_modal.style.display = "none";
     }
 }
 
@@ -137,133 +141,7 @@ inbox_button.onclick = function() {
     message("error", "testing");
 }
 
-/* These are used by reply/reply ll/forward functions below */
-function hide_read_email_buttons(){
-	//Hide the buttons that are no longer relevant
-	document.getElementById("delete_btn").style.display = "none";
-	document.getElementById("reply_btn").style.display = "none";
-	document.getElementById("reply_all_btn").style.display = "none";
-	document.getElementById("forward_btn").style.display = "none";
-}
-function show_read_email_buttons(){
-	//Hide the buttons that are no longer relevant
-	document.getElementById("delete_btn").style.display = "inline";
-	document.getElementById("reply_btn").style.display = "inline";
-	document.getElementById("reply_all_btn").style.display = "inline";
-	document.getElementById("forward_btn").style.display = "inline";
-}
 
-/* Reply stuff */
-function show_reply_form(){
-	hide_read_email_buttons();
-	hide_forward_form();
-	hide_reply_all_form();
-
-	//Display the reply text area
-	document.getElementById("reply_content").style.display = "inline";
-
-	//Display the reply buttons
-	document.getElementById("send_reply_btn").style.display = "inline";
-	document.getElementById("cancel_reply_btn").style.display = "inline";
-}
-
-var reply_button = document.getElementById('reply_btn');
-reply_button.onclick = function() {
-	show_reply_form();
-}
-
-function hide_reply_form(){
-	show_read_email_buttons();
-	//Display the reply text area
-	document.getElementById("reply_content").style.display = "none";
-	//Hide all the stuff we shown for the reply option
-	//Display the reply buttons
-	document.getElementById("send_reply_btn").style.display = "none";
-	document.getElementById("cancel_reply_btn").style.display = "none";
-}
-
-var cancel_reply_button = document.getElementById('cancel_reply_btn');
-cancel_reply_button.onclick = function() {
-	hide_reply_form();
-}
-
-/* Reply all stuff */
-function show_reply_all_form(){
-	hide_read_email_buttons();
-	hide_forward_form();
-	hide_reply_form();
-	//Display the reply text area
-	document.getElementById("reply_all_content").style.display = "inline";
-
-	//Display the reply buttons
-	document.getElementById("send_reply_all_btn").style.display = "inline";
-	document.getElementById("cancel_reply_all_btn").style.display = "inline";
-}
-
-var reply_all_button = document.getElementById('reply_all_btn');
-reply_all_button.onclick = function() {
-	show_reply_all_form();
-}
-
-function hide_reply_all_form(){
-	show_read_email_buttons();
-	//Display the reply text area
-	document.getElementById("reply_all_content").style.display = "none";
-	//Hide all the stuff we shown for the reply option
-	//Display the reply buttons
-	document.getElementById("send_reply_all_btn").style.display = "none";
-	document.getElementById("cancel_reply_all_btn").style.display = "none";
-}
-
-var cancel_reply_all_button = document.getElementById('cancel_reply_all_btn');
-cancel_reply_all_button.onclick = function() {
-	hide_reply_all_form();
-}
-
-function hide_reply_all_form(){
-	show_read_email_buttons();
-	hide_forward_form();
-	hide_reply_form();
-	//Display the reply text area
-	document.getElementById("reply_all_content").style.display = "none";
-	//Hide all the stuff we shown for the reply option
-	//Display the reply buttons
-	document.getElementById("send_reply_all_btn").style.display = "none";
-	document.getElementById("cancel_reply_all_btn").style.display = "none";
-}
-
-/* Forward stuff */
-function show_forward_form() {
-	hide_read_email_buttons();
-
-	//Display the reply text area
-	document.getElementById("forward_content").style.display = "inline";
-
-	//Display the reply buttons
-	document.getElementById("send_forward_btn").style.display = "inline";
-	document.getElementById("cancel_forward_btn").style.display = "inline";
-}
-
-var forward_button = document.getElementById('forward_btn');
-forward_button.onclick = function() {
-	show_forward_form();
-}
-
-function hide_forward_form() {
-	show_read_email_buttons()
-
-	//Display the reply text area
-	document.getElementById("forward_content").style.display = "none";
-	//Hide all the stuff we shown for the reply option
-	//Display the reply buttons
-	document.getElementById("send_forward_btn").style.display = "none";
-	document.getElementById("cancel_forward_btn").style.display = "none";
-}
-
-var cancel_forward_button = document.getElementById('cancel_forward_btn');
-cancel_forward_button.onclick = function() {
-	hide_forward_form();
-}
 
 
 
