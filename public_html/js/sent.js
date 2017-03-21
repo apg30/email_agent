ac2d05487af7b572873c16af8fa218d572fa2c5c
Cameron A. Craig <camieac@gmail.com>
2017-03-21 22:59:13 +0000

    Fixed duplicate ids, started fixing validation buttons

--- public_html/js/sent.js
+++ public_html/js/sent.js
@@ -12,7 +12,7 @@ function get_sent_row_html(email) {
     var email_html = "";
     email_html += '<tr class="read_email">';
     email_html += '<td class = "read_email select"><div class="email-checkbox">';
-    email_html += '<input type="checkbox" value="1" id="email-checkbox1" name="select" />';
+    email_html += '<input type="checkbox" value="1" id="email-checkbox-' + email.message_id + '" name="select" />';
     email_html += '</div></td>';
     email_html += '<td class="read_email from">';
     email_html += email.from;
