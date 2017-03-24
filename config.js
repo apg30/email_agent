0971f20c88fbfad588341a71cbcf127d3981de46
Cameron A. Craig <camieac@gmail.com>
2017-03-24 15:36:33 +0000

    Added carriage return new line.

--- config.js
+++ config.js
@@ -2,10 +2,15 @@
 // ========
 
 module.exports = {
-	mongodb_username: "admin",
-	mongodb_password: "hunter14",
-	mongodb_server : "localhost",
+	mongodb_username: "username",
+	mongodb_password: "password",
+	mongodb_server : "mongo-server-1",
 	mongodb_port: 27017,
+	
+	pop3_host: "outlook.office365.com",
+	pop3_port: 995,
+	pop3_password: "password",
+	pop3_username: "username",
 
 	port: 3000
 
