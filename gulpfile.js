5f1b8ec9f2944f3d118629ff2383ce113c50af92
Cameron A. Craig <camieac@gmail.com>
2017-03-26 00:54:34 +0000

    Added validity check.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
+// gulpfile.js
+// ========
+
 var gulp = require("gulp");
 var ejs = require("gulp-ejs");
 var htmlv = require("gulp-html-validator");
+var Filehound = require("filehound");
+var fs = require("fs");
+var gulps = require("gulp-series");
 var config = require("./config.js")
 
 var parsed_folder = "parsed";
@@ -25,4 +31,38 @@ gulp.task('validate', ['ejs_parse'], function () {
     .pipe(gulp.dest(config.build_dest + validation_folder));
 });
 
-gulp.task('test', ['ejs_parse', 'validate']);
+gulp.task('check', ['validate'], function () {
+	var files = Filehound.create()
+		.ext('ejs')
+		.paths(config.build_dest + validation_folder)
+		.find();
+
+	var success = true;
+	files.then(function proc(files){
+		for(var i = 0; i < files.length; i++){
+			var file_success = true;
+			console.log("Checking " + files[i]);
+			var data = JSON.parse(fs.readFileSync(files[i]));
+			for (var j = 0; j < data.messages.length; j++){
+				if(typeof data.messages[j].type != 'undefined'){
+					console.log(data.messages[j]);
+					if(data.messages[j].type == "error"){
+						console.log("\tError found");
+						success = false;
+						file_success = false;
+					}
+				}
+			}
+			if(file_success){
+				console.log("\tOK");
+			}
+		}
+
+		//Ensure the exit code reflects the success of the validation
+		if(!success){
+			process.exit(1);
+		}
+	});
+});
+
+gulp.task('test', gulp.series('ejs_parse', 'validate', 'check'));
