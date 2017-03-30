ee0a9d9e0c5f96da90178e9b4ea1d1c8785bd509
Cameron A. Craig <camieac@gmail.com>
2017-03-30 10:57:19 +0100

    POP3 lib small cleanup

--- default-config.js
+++ default-config.js
@@ -7,11 +7,6 @@ module.exports = {
 	mongodb_server : "mongo-server-1",
 	mongodb_port: 27017,
 
-	pop3_host: "outlook.office365.com",
-	pop3_port: 995,
-	pop3_password: "password",
-	pop3_username: "username",
-
 	port: 3000,
 
 	build_dest: "./build/"
